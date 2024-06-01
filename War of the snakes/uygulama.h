c99082b976892c77724fcab71cb87208691ae603
OCaglar <96214456+OCaglar@users.noreply.github.com>
2024-06-01 13:13:24 +0300

    Add files via upload

---
+++ War of the snakes/uygulama.h
@@ -0,0 +1,20 @@
+#ifndef UYGULAMA_H_
+#define UYGULAMA_H_
+
+#include<conio.h>
+#include<stdlib.h>
+#include<iostream>
+#include<cstdlib>
+#include"unistd.h"
+using namespace std;
+
+class uygulama  {
+public:	
+	
+	void showinfos99();
+};
+
+
+
+
+#endif
